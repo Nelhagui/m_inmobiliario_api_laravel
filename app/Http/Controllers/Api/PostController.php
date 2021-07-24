118a90605c8f4def0085ba258b2afab1312408e6
Nelson <nelson.aguiar@reiatsu.com.ar>
2021-07-24 02:26:37 -0300

    Nueva ruta para consultas de búsuqedas

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Category;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
@@ -14,6 +15,20 @@ class PostController extends Controller
         $post = Post::where('permanent_link', $url)->get()->first();
         $comments = $post->comments;
         return $post;
+    }
+
+    public function busqueda(Request $request)
+    {
+        // dd($request->get('entrada') == null);
+        $param = $request->get('entrada');
+        if($param !== null){
+            $result = Post::where('title', 'like', '%'.$param.'%')
+                            ->orWhere('permanent_link', 'like', '%'.$param.'%')
+                            ->paginate(15);
+        } else{
+            $result = Post::orderBy('id', 'desc')->paginate(25);
+        }
+        return $result;
     }
     
     public function showCategory($id)
